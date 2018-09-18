bfaf8a139989d7d092698284c9d7741e43f0ff5e
yin5th <541304803@qq.com>
2018-09-18 14:58:34 +0800

    graceful

--- gin_blog/main.go
+++ gin_blog/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"dothis.top/example/gin_blog/pkg/setting"
 	"dothis.top/example/gin_blog/routers"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -18,5 +23,25 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	s.ListenAndServe()
+	//热重启
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("server exiting")
 }
